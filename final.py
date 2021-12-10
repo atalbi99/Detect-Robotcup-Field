8982d13f77699b3906695eb4c24067844647413d
AyaBouzbib <74148323+AyaBouzbib@users.noreply.github.com>
2021-12-10 16:12:41 +0100

    Add files via upload
    Final commit

--- final.py
+++ final.py
@@ -4,40 +4,23 @@ import numpy as np
 from matplotlib.colors import hsv_to_rgb
 
 def erase_component(mask):
-    #The method receives an binary image
-    #find all your connected components
-    #connectedComponentswithStats yields every seperated component with information on each of them, such as size
+    # The method receives an binary image, find all your connected components
+    # ConnectedComponentswithStats yields every seperated component with information on each of them, such as size
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity=8)
-    #the following part is just taking out the background which is also considered a component, but most of the time we don't want that.
+    # The following part is just taking out the background which is also considered a component, but most of the time we don't want that.
     sizes = stats[1:, -1]; nb_components = nb_components - 1
 
-    # minimum size of particles we want to keep (number of pixels)
+    # Minimum size of particles we want to keep (number of pixels)
     min_size = 20000
 
-    #your answer image
     img2 = np.zeros((output.shape))
-    #for every component in the image, you keep it only if it's above min_size
+    # Fo every component in the image, you keep it only if it's above min_size
     for i in range(0, nb_components):
         if sizes[i] >= min_size:
             img2[output == i + 1] = 255
 
-    #kernel = np.ones((12,12),np.uint8)
-    #dilation = cv2.dilate(img2,kernel,iterations = 1)
-    #cv2.imshow('masked', img2)
     return img2
-""""
-def FillHole(mask):
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    len_contour = len (contours)
-    contour_list = []
-    for i in range(len_contour):
-        drawing = np.zeros_like(mask, np.uint8)  # create a black image
-        img_contour = cv2.drawContours(drawing, contours, i, (255, 255, 255), -1)
-        contour_list.append(img_contour)
 
-    out = sum(contour_list)
-    return out
-""" 
 def FillHole(mask):
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     len_contour = len (contours)
@@ -52,8 +35,6 @@ def FillHole(mask):
     for i in range(len(contours)):
         hull = cv2.convexHull(contours[i])
         cv2.drawContours(out, [hull], -1, (255, 0, 0), 2)
-    # Display the final convex hull image
-    #cv2.imshow('ConvexHull', out)
     return out
 
 def contour(image):
@@ -87,58 +68,60 @@ def contour(image):
     cv2.drawContours(img, contours, -1, (0, 0, 0), 15)
     return img
 
+def dice_coef(img, img2):
+        if img.shape != img2.shape:
+            raise ValueError("Shape mismatch: img and img2 must have to be of the same shape.")
+        else:
+            
+            lenIntersection=0
+            
+            for i in range(img.shape[0]):
+                for j in range(img.shape[1]):
+                    if ( np.array_equal(img[i][j],img2[i][j]) ):
+                        lenIntersection+=1
+             
+            lenimg=img.shape[0]*img.shape[1]
+            lenimg2=img2.shape[0]*img2.shape[1]  
+            value = (2. * lenIntersection  / (lenimg + lenimg2))
+        return value
 
-img_number = 64
+L1 = [10,20,25,53,62,68,79,86,95,98,103,105,109,271,279,330]
+L2 = [18,33,37,43,53,55,89,154,193,198,201,236]
+L3 = [1,11,15,72,96,150,163,167,208,217]
+L4 = [22,24,31,35]
+dice_score =[]
 
-while(img_number < 265) :
+for img_number in L4 :
 
-    cadena = '{0:03d}'.format(img_number)
-    path = 'data/log1/'+str(cadena)+'-rgb.png'
+    cadena = '{0:02d}'.format(img_number) # Use the format 0:03d for L1,L2,L3 
+    path = 'data/log4/'+str(cadena)+'-rgb.png' 
     img2 = cv2.imread(path)
     print(path)
     #cv2.imshow('Image initiale',img2)
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
     img = cv2.medianBlur(img2, 15)
-    #imgb = cv2.bilateralFilter(img2, 15, 75, 75)
 
     light_green = (29, 28, 20)
     dark_green = (68, 234, 255)
     light_green1 = (29, 28, 20)
     dark_green1 = (68, 234, 255)
 
-    #hsv_imgb = cv2.cvtColor(imgb, cv2.COLOR_RGB2HSV)
+    
     hsv_imgb = cv2.cvtColor(img2, cv2.COLOR_RGB2HSV)
 
     mask1 = cv2.inRange(hsv_imgb, light_green, dark_green)
-    #print(mask1.dtype)
-    #cv2.imshow('mask1', mask1)
-    #mask2 = cv2.inRange(hsv_img, light_green, dark_green)
     mask2 = cv2.inRange(hsv_imgb, light_green1, dark_green1)
-    #cv2.imshow('mask2', mask2)
     mask = mask1 + mask2
-    #cv2.imshow('mask', mask)
-
-    #result = np zeros ..
-    #result[mask2]=img[mask2]
     result =  cv2.bitwise_and(img, img, mask=mask1)
-    #cv2.imshow('result',result)
-    #img=imgb
     img=img2
 
     # File hole
     mask_out = FillHole(mask)
     mask_out1 = FillHole(mask_out)
-    #cv2.imshow('maskb', mask_out1)
     # Erase component
     erased = erase_component(mask_out1)
-    # cv2.imshow('erased.png',erased)
     cv2.imwrite('erased.png',erased)
-    #cv2.waitKey(0)  
-    #cv2.destroyAllWindows()
 
-    # Detection de contour
-
-    #edit the filename based on the inpput name of the file
     filename = 'erased.png'
     t = 10
     image1 = contour(filename)
@@ -146,8 +129,6 @@ while(img_number < 265) :
 
     imageb = cv2.imread('out2.png')
     imageb = cv2.cvtColor(imageb, cv2.COLOR_BGR2GRAY)
-
-    #identify rect
 
     medianb = cv2.medianBlur(imageb, 15)
 
@@ -158,31 +139,19 @@ while(img_number < 265) :
     image_gray = np.where(image_gray > 30, 255, image_gray)
     image_gray = np.where(image_gray <= 30, 0, image_gray)
 
-    contours,hierachy = cv2.findContours(image_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    rect_cnts = []
-    for cnt in contours:
-        peri = cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, 0.04 * peri, True)
-
-        if len(approx) == 4: # shape filtering condition
-            rect_cnts.append(cnt)
-
-    max_area = 0
-    football_square = None
-    for cnt in rect_cnts:
-        (x, y, w, h) = cv2.boundingRect(cnt)
-        if max_area < w*h:
-            max_area = w*h
-            football_square = cnt
-
     # Draw the result
     image = cv2.cvtColor(image_gray, cv2.COLOR_GRAY2RGB)
-   # cv2.drawContours(image, [football_square], -1, (0, 0,255), 5)
 
-    #cv2.imshow('Result Preview.png', image)
-    #cv2.imwrite('result.png', image)
-    path_result = 'data_result/log1/'+str(cadena)+'-rgb_result.png'
+
+    path_result = 'data_result/log4/'+str(cadena)+'-rgb_result.png'
+    path_test = 'mask-field/log4/'+str(cadena)+'-rgb.png'
     cv2.imwrite(path_result,image)
+    image_test = cv2.imread(path_test)
+
+    value = dice_coef(image, image_test)
+    dice_score.append(value)
     img_number += 1
     cv2.waitKey()
     cv2.destroyAllWindows()
+
+print(dice_score)
